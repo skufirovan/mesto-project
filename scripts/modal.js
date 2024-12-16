8b919b836b94170e816af682dce41593fa738cb8
unknown <khaspalaevan05@gmail.com>
2024-12-17 00:45:56 +0300

    feat: закрыть попап через esc и нажатие на оверлей

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,17 +1,30 @@
 const popup = document.querySelectorAll('.popup');
 
-function openModal(popup) {      
+function openModal(popup) { 
+    document.addEventListener('keydown', closeByEsc); 
     popup.classList.add('popup_is-opened');
 }
 
-function closeModal(popup) {      
+function closeModal(popup) {   
+    document.removeEventListener('keydown', closeByEsc);   
     popup.classList.remove('popup_is-opened');
+}
+
+function closeByEsc(evt) {
+    if (evt.key === 'Escape') {
+        const openedPopup = document.querySelector('.popup_is-opened');
+        if (openedPopup !== null) {
+            closeModal(openedPopup);
+        }
+    }
 }
 
 popup.forEach(function(ind) {
     ind.classList.add('popup_is-animated');
     ind.addEventListener('click', function(evt) {
         if (evt.target.classList.contains('popup__close')) {
+            closeModal(evt.currentTarget);
+        } else if (evt.target.classList.contains('popup')) {
             closeModal(evt.currentTarget);
         }
     })
